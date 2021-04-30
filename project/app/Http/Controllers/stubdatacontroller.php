a8359f179a93cb45da406becd73d104670d59de2
Rasheed Ur Rehman <56723562+Rasheed-ur-Rehman@users.noreply.github.com>
2021-04-30 11:16:53 +0500

    stub and seeders

---
+++ project/app/Http/Controllers/stubdatacontroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class stubdatacontroller extends Controller
+{
+    function adddata()
+    {
+    	return "Data Is Sucessfully Added ";
+    }
+}
